0f5517827363f6e627fe0efcf9c788de49df9346
ray <ray@yeah.net>
2018-12-19 13:05:16 +0800

    修改不获取body 获取html

--- src/lightbox.vue
+++ src/lightbox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="vue-images">
-    <div style="height:999px"></div>
+    <!-- <div style="height:999px"></div> -->
     <gallery :images="images" @changeIndex="changeImg($event)"></gallery>
     <div ref="lightbox" class="lightbox" v-show="isShow" @click="isShow=!modalclose">
       <fancybox ref="fancybox" :images="images" :index="index" :reset="!isShow" @play="playImg" @pause="pauseImg" @close="closeImg" @addIndex="nextImg" @decIndex="prevImg" :showclosebutton="showclosebutton" :showcaption="showcaption" :imagecountseparator="imagecountseparator" :showimagecount="showimagecount"></fancybox>
@@ -217,7 +217,8 @@
         if (this.isShow) {
           // console.log('进来了')
           // document.body.style.position = 'fixed'
-          document.body.style.overflowY = 'hidden'
+          let html = document.getElementsByTagName("html");
+		      html[0].style.overflowY="hidden"
           window.addEventListener('keydown', this.keyFun)
           this.$refs.lightbox.addEventListener('mousewheel', this.wheelFun)
           this.$refs.lightbox.addEventListener('touchstart', this.touchFun)
@@ -226,7 +227,9 @@
           this.pauseImg()
           // console.log('进来了2')
           // document.body.style.position = 'static'
-          document.body.style.overflowY = 'auto'
+          // document.body.style.overflowY = 'auto'
+          let html = document.getElementsByTagName("html");
+		      html[0].style.overflowY="auto"
           window.removeEventListener('keydown', this.keyFun)
           this.$refs.lightbox.removeEventListener('mousewheel', this.wheelFun)
           this.$refs.lightbox.removeEventListener('touchstart', this.touchFun)
